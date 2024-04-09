6d9b362cadb91ee628d78b2aa0e16b56ee4ce6f9
Hamza Haque <hamzahaque30@gmail.com>
2024-04-09 00:23:29 -0500

    print data line by line

--- app/App.php
+++ app/App.php
@@ -4,5 +4,10 @@ declare(strict_types=1);
 
 // Your Code
 $transactionFile = fopen("../transaction_files/sample_1.csv", "r") or die("Unable to open file!");
-echo fread($transactionFile, filesize("../transaction_files/sample_1.csv"));
+// echo fread($transactionFile, filesize("../transaction_files/sample_1.csv"));
+
+while (!feof($transactionFile)) { // while the file pointer is not at the end of the file
+  echo fgets($transactionFile) . "<br />";
+}
+
 fclose($transactionFile);
